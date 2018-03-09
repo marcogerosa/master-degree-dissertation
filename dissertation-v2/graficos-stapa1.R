4dbb31cdc9f73c4494ccd6afe586f1e0c770e5f2
SuelenGC <suelengcarvalho@gmail.com>
2018-03-09 00:22:45 -0300

    Fixes after defesa

--- dissertation-v2/graficos-stapa1.R
+++ dissertation-v2/graficos-stapa1.R
@@ -1,20 +1,26 @@
+library(ggplot2)
+library(dplyr)
+library(reshape2)
+library(forcats)
+
+
 df <- read.table(text="Elemento   Questao  Tipo   Total
-                        Activity  Q1  'Boa Pr\uE1ticas' 35
-                        Activity  Q2  'Ma Pr\uE1ticas'  38
-                        Fragment  Q3  'Boa Pr\uE1ticas' 30
-                        Fragment  Q4  'Ma Pr\uE1ticas'  27
-                        Adapter   Q5  'Boa Pr\uE1ticas' 25
-                        Adapter   Q6  'Ma Pr\uE1ticas'  21
-                        Listener  Q7  'Boa Pr\uE1ticas' 10
-                        Listener  Q8  'Ma Pr\uE1ticas'  11
-                        Layout    Q9  'Boa Pr\uE1ticas' 33
-                        Layout    Q10 'Ma Pr\uE1ticas'  22
-                        Style     Q11 'Boa Pr\uE1ticas' 12
-                        Style     Q12 'Ma Pr\uE1ticas'  9
-                        String    Q13 'Boa Pr\uE1ticas' 23
-                        String    Q14 'Ma Pr\uE1ticas'  19
-                        Drawable  Q15 'Boa Pr\uE1ticas' 22
-                        Drawable  Q16 'Ma Pr\uE1ticas'  9", header=TRUE)
+                        Activity  Q1  'Boas Pr\uE1ticas' 35
+                        Activity  Q2  'M\uE1s Pr\uE1ticas'  38
+                        Fragment  Q3  'Boas Pr\uE1ticas' 30
+                        Fragment  Q4  'M\uE1s Pr\uE1ticas'  27
+                        Adapter   Q5  'Boas Pr\uE1ticas' 25
+                        Adapter   Q6  'M\uE1s Pr\uE1ticas'  21
+                        Listener  Q7  'Boas Pr\uE1ticas' 10
+                        Listener  Q8  'M\uE1s Pr\uE1ticas'  11
+                        Layout    Q9  'Boas Pr\uE1ticas' 33
+                        Layout    Q10 'M\uE1s Pr\uE1ticas'  22
+                        Style     Q11 'Boas Pr\uE1ticas' 12
+                        Style     Q12 'M\uE1s Pr\uE1ticas'  9
+                        String    Q13 'Boas Pr\uE1ticas' 23
+                        String    Q14 'M\uE1s Pr\uE1ticas'  19
+                        Drawable  Q15 'Boas Pr\uE1ticas' 22
+                        Drawable  Q16 'M\uE1s Pr\uE1ticas'  9", header=TRUE)
 
 df <- melt(df, id.var=c("Questao", "Elemento", "Tipo", "Total"))
 
